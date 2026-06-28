337a2bd6a1553b9f3c2a0d67a957e531e683ba26
Dhairya Rastogi <dhairyarastogi1411@gmail.com>
2026-06-28 13:55:01 +0530

    Add View Resume button opening in new tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,27 +76,41 @@ export default function Home() {
             </Text>
           </RevealFx>
           <RevealFx paddingTop="12" delay={0.4} horizontal="center" paddingLeft="12">
-            <Button
-              id="about"
-              data-border="rounded"
-              href={about.path}
-              variant="secondary"
-              size="m"
-              weight="default"
-              arrowIcon
-            >
-              <Row gap="8" vertical="center" paddingRight="4">
-                {about.avatar.display && (
-                  <Avatar
-                    marginRight="8"
-                    style={{ marginLeft: "-0.75rem" }}
-                    src={person.avatar}
-                    size="m"
-                  />
-                )}
-                {about.title}
-              </Row>
-            </Button>
+            <Row gap="16" horizontal="center" wrap>
+              <Button
+                id="about"
+                data-border="rounded"
+                href={about.path}
+                variant="secondary"
+                size="m"
+                weight="default"
+                arrowIcon
+              >
+                <Row gap="8" vertical="center" paddingRight="4">
+                  {about.avatar.display && (
+                    <Avatar
+                      marginRight="8"
+                      style={{ marginLeft: "-0.75rem" }}
+                      src={person.avatar}
+                      size="m"
+                    />
+                  )}
+                  {about.title}
+                </Row>
+              </Button>
+              <Button
+                id="resume"
+                data-border="rounded"
+                href="/Dhairya_Rastogi_Resume.pdf"
+                target="_blank"
+                variant="primary"
+                size="m"
+                weight="default"
+                suffixIcon="openLink"
+              >
+                View Resume
+              </Button>
+            </Row>
           </RevealFx>
         </Column>
       </Column>
